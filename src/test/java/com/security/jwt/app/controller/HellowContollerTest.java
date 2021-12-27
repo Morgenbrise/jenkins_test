339df97e20dad0abe43afefe300e63ff56749f7a
sj <ischos02@gmila.com>
2021-12-27 20:58:23 +0900

    Junit 추가

---
+++ src/test/java/com/security/jwt/app/controller/HellowContollerTest.java
@@ -0,0 +1,14 @@
+package com.security.jwt.app.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HellowContollerTest {
+
+    @Test
+    public void 테스트() {
+        System.out.println("테스트1");
+    }
+
+}
